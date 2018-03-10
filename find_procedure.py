745dc10b3fbc505b8adba319ef932a3782cae8d8
Ilya Bazhenov <31971067+bazhil@users.noreply.github.com>
2018-03-10 22:36:10 +0300

    Add files via upload

--- find_procedure.py
+++ find_procedure.py
@@ -1,39 +1,16 @@
-# Задание
-# мне нужно отыскать файл среди десятков других
-# я знаю некоторые части этого файла (на память или из другого источника)
-# я ищу только среди .sql файлов
-# 1. программа ожидает строку, которую будет искать (input())
-# после того, как строка введена, программа ищет её во всех файлах
-# выводит список найденных файлов построчно
-# выводит количество найденных файлов
-# 2. снова ожидает ввод
-# поиск происходит только среди найденных на этапе 1
-# 3. снова ожидает ввод
-# ...
-# Выход из программы программировать не нужно.
-# Достаточно принудительно остановить, для этого можете нажать Ctrl + C
+import os
 
-# Пример на настоящих данных
-
-# python3 find_procedure.py
-# Введите строку: INSERT
-# ... большой список файлов ...
-# Всего: 301
-# Введите строку: APPLICATION_SETUP
-# ... большой список файлов ...
-# Всего: 26
-# Введите строку: A400M
-# ... большой список файлов ...
-# Всего: 17
-# Введите строку: 0.0
-# Migrations/000_PSE_Application_setup.sql
-# Migrations/100_1-32_PSE_Application_setup.sql
-# Всего: 2
-# Введите строку: 2.0
-# Migrations/000_PSE_Application_setup.sql
-# Всего: 1
-
-# не забываем организовывать собственный код в функции
+# migrations = 'Migrations'
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+#
+# folder = os.path.abspath(os.path.dirname('C:\Users\magistr\Documents\netology courses\Python_course-master\homework\2.3-paths\Migrations\'))
+# counter = None
+# if __name__ == '__main__':
+#     for file in folder:
+#         if file.endswith('.sql'):
+#             counter+=1
+#     print(counter)
+#     pass
 
 import os
 
@@ -56,9 +33,9 @@ def find_file():
             file_list = result
             result = []
         for file in file_list:
-            with open(abs_path + '\\' + file, 'r') as f:
+            with open(abs_path + '\' + file, 'r') as f:
                 for line in f:
-                    if string in f.read():
+                    if string in line:
                         result.append(file)
                         break
         for res in result:
@@ -67,5 +44,4 @@ def find_file():
 
 if __name__ == '__main__':
     find_file()
-
-
+    pass
